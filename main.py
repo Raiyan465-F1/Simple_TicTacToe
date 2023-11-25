9b97f0419a98657b76ca6ad1319646e821f60631
Raiyan Rahman <72751843+Raiyan465-F1@users.noreply.github.com>
2023-11-25 15:07:04 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from Advanced_tictac import Tictactoe
+
+tttoe = Tictactoe()
+
+def PlayGame():
+    while True:
+        # PLAYER 1S MOVE
+        tttoe.display_board()
+
+        print("Player 1's move: X")
+        player1:str = tttoe.get_input()
+
+        #Updating board to new move
+        tttoe.update_board("X", player1)
+        tttoe.display_board()
+
+        # Checking for winner
+        if tttoe.check_for_winner():
+            print("Player1 wins")
+            break
+        # PLAYER 2S MOVE
+        print("Player 2's move: O")
+        player2:str = tttoe.get_input()
+
+        # Updating board to new move
+        tttoe.update_board("O", player2)
+        
+        #checking for winner
+        if tttoe.check_for_winner():
+            tttoe.display_board()
+            print("Player2 wins")
+            break
+
+PlayGame()
